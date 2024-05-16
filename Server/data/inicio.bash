0d4c12b3292dbe6efc86c4350b1452c06d677062
gacob <josemiguel_pilas@hotmail.com>
2024-05-16 15:01:52 +0200

    Añadido Wait Room. Fix para la asignación de IP y comienzo del combate

--- Server/data/inicio.bash
+++ Server/data/inicio.bash
@@ -4,7 +4,7 @@ source data/first_turn.bash
 source data/second_turn.bash
 
 inicio() {
-
+    
    # Contador para asignar IP a jugadores
    case $count in
     1)  cliente_ip_1=$NCAT_REMOTE_ADDR
@@ -12,22 +12,24 @@ inicio() {
         echo Esta es tu IP: "$cliente_ip_1"
         count=$(( count + 1 ))
         ;;
-    2) cliente_ip_2=$NCAT_REMOTE_ADDR
-        cliente_ip_2="${cliente_ip_2//./}"
-        echo Esta es tu IP: "$cliente_ip_2"
-        count=$(( count + 1 ))
-        ;;
    esac
+
+    # Wait Room
+    echo Esperando a los dos jugadores...
 
     # Almacenamos las IP para Debug
     echo $cliente_ip_1 >> list_ip
-    echo $cliente_ip_2 >> list_ip
+    cliente_ip_1=$( sed -n 2p list_ip )
+    while [[ $cliente_ip_1 != "" && $cliente_ip_2 == "" ]]; do
+        cliente_ip_2=$( sed -n 3p list_ip )
+    done
+
     echo "ip 1" $cliente_ip_1
     echo "ip 2" $cliente_ip_2
 
     # Turno aleatorio. Se sale del turno al morir
     randomturn=$(( RANDOM%1 ))
-    while [[ $first_hp -gt 0 && $second_hp -gt 0 ]]; do
+    while [[ $first_hp -gt 0 && $second_hp -gt 0 && $cliente_ip_1 != "" && $cliente_ip_2 != "" ]]; do
         if [[ $randomturn -eq 0 ]]; then
             first_turn
             second_turn

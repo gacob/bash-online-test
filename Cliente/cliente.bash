4a0a1058bbf690c1ce78549f31401502970022d1
gacob <josemiguel_pilas@hotmail.com>
2024-05-16 15:01:19 +0200

    Preparación del Cliente

---
+++ Cliente/cliente.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ip=0
+puerto=0
+
+# echo ¿A qué IP quieres conectarte?
+# read -r ip
+
+# echo ¿A qué puerto quieres conectarte?
+# read -r puerto
+
+# nc "$ip" "$puerto"
+
+# Conexión
+nc 25.49.98.246 8080
+

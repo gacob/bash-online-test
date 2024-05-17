c509302ff9bae1b0f35c3c7ae8a196e0ca7af223
gacob <josemiguel_pilas@hotmail.com>
2024-05-17 18:49:22 +0200

    Final Touches

--- Server/data/first_turn.bash
+++ Server/data/first_turn.bash
@@ -8,9 +8,11 @@ first_turn() {
     combat=1
     my_damage=0
 
-    touch combat_data 
+    rm ../combat_data
+    touch ../combat_data
+    echo $combat >> ../combat_data
 
-    # Turno de Jugador 1
+    # Turno del Jugador 1
     if [[ "$current" -eq "$cliente_ip_1" ]]; then
         echo "Turno del Jugador 1"
         echo ¿Cuánto daño vas a hacer?
@@ -20,25 +22,25 @@ first_turn() {
         echo Has hecho "$my_damage de daño" 
         echo "Al Jugador 2 le queda $second_hp de vida."
 
-        echo "$my_damage" >> combat_data
-        echo "$second_hp" >> combat_data
+        echo $my_damage >> ../combat_data
+        echo $second_hp >> ../combat_data
         my_damage=0
         combat=0
-        echo $combat >> combat_data
+        echo $combat > $( sed -n 1p ../combat_data )
     # Espera de Jugador 2
     else
         echo Es el turno del rival
 
         # Obtener data del combate
         while [[ $combat -eq 1 ]]; do
-            my_damage=$( sed -n 1p combat_data )
-            second_hp=$( sed -n 2p combat_data )
-            combat=$( sed -n 3p combat_data )
+            my_damage=$( sed -n 2p ../combat_data )
+            second_hp=$( sed -n 3p ../combat_data )
+            combat=$( sed -n 1p ../combat_data )
 
         done
 
         echo El enemigo te ha hecho "$my_damage" de daño.
         echo Te queda "$second_hp" de vida.
-        rm combat_data
+        rm ../combat_data
     fi
 }
